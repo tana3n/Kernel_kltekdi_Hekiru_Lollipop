ce24cd3bcba21282adc0e6ec593513552832a1da
DerRomtester <dieb.stefan.96@gmail.com>
2014-10-17 15:33:26 +0200

    introduce: alucard hotplug
    Conflicts:
    	arch/arm/mach-msm/Makefile
    
    Conflicts:
    	arch/arm/configs/msm8974_sec_defconfig

--- build_kernel_sample.sh
+++ build_kernel_sample.sh
@@ -6,7 +6,7 @@ export CROSS_COMPILE=/home/koyomi/rino/arm-eabi-4.7/bin/arm-eabi-
 mkdir output
 
 make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_klte_kdi_defconfig SELINUX_DEFCONFIG=selinux_defconfig
-make -C $(pwd) O=output
+make -C $(pwd) O=output -j 12
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
 
