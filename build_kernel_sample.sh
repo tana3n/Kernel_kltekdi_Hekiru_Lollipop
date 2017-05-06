f7da6d4777101e5694f581f87b37cd432e771ca8
tana3n <dorach.com@gmail.com>
2017-05-06 12:09:50 +0900

    klte_Hekiru_lollpop_v0.1

--- build_kernel_sample.sh
+++ build_kernel_sample.sh
@@ -2,7 +2,8 @@
 
 export ARCH=arm
 #export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
-export CROSS_COMPILE=/home/koyomi/rino/arm-eabi-4.7/bin/arm-eabi-
+#export CROSS_COMPILE=/home/koyomi/rino/arm-eabi-4.7/bin/arm-eabi-
+export CROSS_COMPILE=/home/koyomi/rino/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 mkdir output
 
 make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_klte_kdi_defconfig SELINUX_DEFCONFIG=selinux_defconfig
