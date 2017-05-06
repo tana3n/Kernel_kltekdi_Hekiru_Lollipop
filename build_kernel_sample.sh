85733b02af1934f6d6244d9ae79d452d02e59197
tana3n <dorach.com@gmail.com>
2017-05-06 16:25:01 +0900

    Changes to be committed:

--- build_kernel_sample.sh
+++ build_kernel_sample.sh
@@ -2,12 +2,12 @@
 
 export ARCH=arm
 #export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
-#export CROSS_COMPILE=/home/koyomi/rino/arm-eabi-4.7/bin/arm-eabi-
-export CROSS_COMPILE=/home/koyomi/rino/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export CROSS_COMPILE=/home/koyomi/rino/arm/arm-eabi-4.8/bin/arm-eabi-
+#export CROSS_COMPILE=/home/koyomi/rino/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 mkdir output
-
 make -C $(pwd) O=output msm8974_sec_defconfig VARIANT_DEFCONFIG=msm8974pro_sec_klte_kdi_defconfig SELINUX_DEFCONFIG=selinux_defconfig
 make -C $(pwd) O=output -j 12
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
+cp /home/koyomi/rino/Kernel_kltekdi/output/arch/arm/boot/zImage /home/koyomi/rino/mkbootimg_tools-master/lp_SCL23_2/zImage
 
